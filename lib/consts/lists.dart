9994c9ab1c90c2dfcc99998ced27bacbc1609aca
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-10-25 00:08:44 +0700

    Featured categories updated

--- lib/consts/lists.dart
+++ lib/consts/lists.dart
@@ -1,7 +1,13 @@
+import 'package:flutter_ecommerce_app/consts/consts.dart';
 import 'package:flutter_ecommerce_app/consts/images.dart';
 
 const socialIconList = [icFacebookLogo, icGoogleLogo];
 
 const slidersList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
-
 const secondSlidersList = [imgSs1, imgSs2, imgSs3, imgSs4];
+
+const featuredImages1 = [imgS1, imgS2, imgS3];
+const featuredImages2 = [imgS4, imgS5, imgS6];
+
+const featuredTitles1 = [womenDress, girlsDresses, girlsWatches];
+const featuredTitles2 = [boysGlasses,mobilePhone,tShirts];
