0c0da3564727771c1bb016ce739272d79832e177
thanhhuydoan2002 <ddoanthanhhuy@gmail.com>
2023-11-18 12:35:31 +0700

    comments update

--- lib/consts/lists.dart
+++ lib/consts/lists.dart
@@ -1,20 +1,27 @@
 import 'package:flutter_ecommerce_app/consts/consts.dart';
 
+//LOGIN SCREEN
 const socialIconList = [icFacebookLogo, icGoogleLogo];
 
+//HOME SCREEN
 const slidersList = [imgSlider1, imgSlider2, imgSlider3, imgSlider4];
 const secondSlidersList = [imgSs1, imgSs2, imgSs3, imgSs4];
 
+//HOME SCREEN
 const featuredImages1 = [imgS1, imgS2, imgS3];
 const featuredImages2 = [imgS4, imgS5, imgS6];
 
+//HOME SCREEN
 const featuredTitles1 = [womenDress, girlsDresses, girlsWatches];
 const featuredTitles2 = [boysGlasses,mobilePhone,tShirts];
 
+//CATEGORY SCREEN
 const categoriesList = [womenClothing,menClothing,compAccess,automobile,kidToys,sports,jewelery,cellPhone,furniture];
 const categoryImages = [imgFc1,imgFc2,imgFc3,imgFc4,imgFc5,imgFc6,imgFc7,imgFc8,imgFc9];
 
+//ITEM DETAILS SCREEN
 const itemDetailButtonsList = [video,reviews,sellerPolicy,returnPolicy,supportPolicy];
 
+//PROFILE SCREEN
 const profileButtonsList = [orders, wishlist, messages];
 const profileButtonsIcon = [icOrders, icOrders, icMessages];
